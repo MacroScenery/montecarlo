2a53e7a79e345ef694552f9c1c436c61602c630e
Sean Lynch <sean04elmo@gmail.com>
2023-11-07 16:17:10 -0500

    Update monte.py

--- monte.py
+++ monte.py
@@ -3,9 +3,6 @@
 import random # use random.random()
 
 class Computer:
-  virus = False
-  wasInfected = False
-  
   def __init__(self, virus=False, wasInfected=False):
     self.virus = virus
     self.wasInfected = wasInfected
@@ -14,83 +11,128 @@ class Computer:
         return "{" + str(self.virus) + ", " + str(self.wasInfected) + "}"
 
 
-## TODO: Rewrite this function by getting indexs of infected pcs and choosing from that group
+def printNetwork(network, headerText=""):
+    print(headerText)
+    print("--------------")
+    print("Index\t| Virus\t| Was Infected")
+    print("--------------")
+    for i in range(len(network)):
+        print(str(i)+ "\t| " + str(network[i].virus) + "\t| " + str(network[i].wasInfected))
+
+    print("--------------\n")
+    return
+
+
 def repairNetwork(network, numberOfRepairs):
-    if (len(network) <= numberOfRepairs):
-        for computer in network:
-            computer.virus = False
-        return network
+ 
+    infectedComputers = [computer for computer in network if computer.virus]
     
-    numberOfInfected = 0
-    for computer in network:
-        if (computer.virus == True):
-            numberOfInfected += 1
-    
-    while (numberOfRepairs > 0 and numberOfInfected > 0): # run repairs until number over or until no infected left
-        choosen = int(random.random() * len(network)) # choose a random computer
-        if (network[choosen].virus == True):
-            network[choosen].virus = False
-            numberOfInfected -= 1
-            numberOfRepairs -= 1
-    
+    while (numberOfRepairs > 0  and len(infectedComputers) > 0):
+        index = int(random.random() * len(infectedComputers))
+        infectedComputers[index].virus = False
+        infectedComputers.pop(index)
+        numberOfRepairs -= 1
+
     return network
 
 def nextStateVirus(network, p):
-    changeLog = [False] * len(network) # creates a lists of false values that is len(network)
-    # false means value has not been changed this state
-    # true means value has been changed
+    infectedComputers = [computer for computer in network if computer.virus]
+    noninfectedComputers = [computer for computer in network if not computer.virus]
 
-    # loop thorugh each computer
-    for i in range(len(network)):
-        # find an infected computer
-        # check if the value was changed during this loop
-        if ((network[i].virus == True) and (changeLog[i] != True)):
-            
-            for j in range(0, i):
-                # move array backwards
-                if (random.random() <= p): # if virus works
-                    network[j].virus = True
-                    if (network[j].wasInfected == False):
-                        network[j].wasInfected = True
-            
-            for k in range(i+1, len(network)):
-                # mov array forwards
-                if (random.random() <= p): # if virus works
-                    network[k].virus = True  
-                    if (network[k].wasInfected == False):
-                        network[k].wasInfected = True       
+    for virus in infectedComputers:
+        for i in range(len(noninfectedComputers)):
+            if random.random() < p:
+                noninfectedComputers[i].virus = True
+                if (noninfectedComputers[i].wasInfected == False):
+                    noninfectedComputers[i].wasInfected = True
 
-    return network
+
+
+
+    newNetwork = infectedComputers + noninfectedComputers
+
+    return newNetwork
+
 
 # Runs through one state of network and returns new network
 # Takes in a network array of booleans, proability of virus p
 def nextState(network, p, numberOfRepairs):
     network = nextStateVirus(network, p)
+    #printNetwork(network, headerText="Post-Virus")
     network = repairNetwork(network, numberOfRepairs)
+    #printNetwork(network, headerText="Post-Repair")
     return network
 
 def simulation(networkSize=20, p=0.1, numberOfRepairs=5):
-    network = [Computer()] * networkSize
+    network = [Computer() for _ in range(networkSize)]
     network[ int(random.random() * networkSize) ] = Computer(True, True) # create network and make one random computer infected
     numberOfInfected = 1
 
+    dayCounter = 0
+
     while (numberOfInfected > 0):
+        #print("Day:" + str(dayCounter))
         numberOfInfected = 0
         network = nextState(network, p, numberOfRepairs)
         for computer in network:
-            print(computer)
             if computer.virus == True:
                 numberOfInfected += 1
         
+        dayCounter += 1
+        
+
+    return (network, dayCounter)
+
+def monteCarlo(n, networkSize=20, p=0.1, numberOfRepairs=5):
+
+    avgNumberOfDaysTillClean = 0.0
+    probabilityInfectOnce = 0.0
+    expectedComputersInfected = 0.0
+
+    for i in range(n):
+        print("Running simulation " + str(i+1))
+        postNetwork, days = simulation(networkSize, p, numberOfRepairs)
+
+        print("Calculating stats " + str(i+1))
+        avgNumberOfDaysTillClean += days
+        
+        for computer in postNetwork:
+            if computer.wasInfected == True:
+                probabilityInfectOnce += 1
+
+    
+    avgNumberOfDaysTillClean /= float(n)
+
+    probabilityInfectOnce /= float(networkSize * n)
+
+    print(avgNumberOfDaysTillClean)
+    print(probabilityInfectOnce)
 
 
-
-
-    return
 
 
 def main():
-    simulation(10)
+    monteCarlo(1000)
+    
+    # network = [Computer() for _ in range(20)]
+    # network[ int(random.random() * 20) ] = Computer(True, True) # create network and make one random computer infected
+    # printNetwork(network, "Pre-Virus")
+    # network = nextStateVirus(network, 0.5)
+    # printNetwork(network, "Post-Virus")
+    # n = 0
+    # for computer in network:
+    #     if computer.virus == True:
+    #         n+=1
+    # print(n)
+
+    # network = repairNetwork(network, 5)
+    # printNetwork(network, "Post-Repair")
+    # n = 0
+    # for computer in network:
+    #     if computer.virus == True:
+    #         n+=1
+    # print(n)
+
 
 
 
